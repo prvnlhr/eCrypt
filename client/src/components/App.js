bc95c8e28cd2ac9e4a637cc0d00db54a8e0c9740
prvnlhr <praveenlohar04@gmail.com>
2021-08-16 16:38:17 +0530

    viewport resized

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -44,6 +44,8 @@ const App = () => {
 
   let vh = window.innerHeight * 0.01;
   // Then we set the value in the --vh custom property to the root of the document
+
+
   document.documentElement.style.setProperty("--vh", `${vh}px`);
   window.addEventListener("resize", () => {
     // We execute the same script as before
